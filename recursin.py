4590d5a301ded8b169199320caf9ba6bfa83864b
mhmdhuss <mhmdhuss44@gmail.com>
2024-01-01 12:23:23 +0200

    space complexity

--- recursin.py
+++ recursin.py
@@ -5,6 +5,12 @@ def factorial_calc(value):
         return 1
     return value*factorial_calc(value-1)
 
+# Space Complexity: O(n)
+# Explanation: The space complexity of is determined by the depth of the stack in recursion
+# and in the Worst Case Scene we have to go n times! (because we reduce 1 each time until we get to 0)
+
+
+
 
 
 def calc_permutations(str):
@@ -16,6 +22,11 @@ def calc_permutations(str):
       for res in calc_permutations(str[:i] + str[i+1:]):
         result += [val + res]
   return result
+
+# Space Complexity: O(n!)
+# Explanation: The space complexity of the function is determined
+# by the number of permutations , and we know that the number of permuatations of a string
+# of len n is n! and each one takes a recursive call , so we have n! stack calls
 
 
 if __name__ == '__main__':
